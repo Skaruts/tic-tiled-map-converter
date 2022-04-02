c9d3641782693789aba50ab5df11f443a78107ae
Skaruts <Skaruts@users.noreply.github.com>
2022-04-02 23:51:00 +0100

    add map->tmx conversion

--- tictiled.py
+++ tictiled.py
@@ -1,11 +1,11 @@
 import sys
 import os
 import xmltodict
-import json
+# import json  # for debugging
 
 # TODO:
-#   optionally care about layer visibility
-#   convert map -> tmx
+#   optionally care about layer visibility in order to ignore layers
+#   account for when no tileset file exists yet
 #   consider asking about overwriting 'dest_file'?
 
 #=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
@@ -35,76 +35,112 @@ def check_ignored_flag(layer):
 	return False
 
 
-def convert(src_file, dest_file):
-	print(f"converting '{src_file}' to '{dest_file}'...")
+def tmx_to_map(src_fname, dest_fname):
+	print(f"converting from '{src_fname}' to '{dest_fname}'...")
+	map_data = [0 for i in range(240*136)]
 
-	src_ext  = os.path.splitext(src_file)[1]
-	dest_ext = os.path.splitext(dest_file)[1]
+	with open(src_fname, 'r') as f:
+		layers = []
+		xml = xmltodict.parse(f.read())
+		# print(json.dumps(xml, indent=4))
 
-	if src_ext == ".tmx":
-		map_data = [0 for i in range(240*136)]
+		# print( isinstance(xml["map"]["layer"], list) )
+		if not isinstance(xml["map"]["layer"], list):
+			layers.append(xml["map"]["layer"])
+		else:
+			for l in xml["map"]["layer"]:
+				layers.append(l)
 
-		with open(src_file, 'r') as f:
-			layers = []
-			xml = xmltodict.parse(f.read())
-			# print(json.dumps(xml, indent=4))
+		# collect tile data from layers
+		layer_data = []
+		for l in layers:
+			ignored = check_ignored_flag(l)
+			if not ignored:
+				data = l["data"]
+				encoding = data["@encoding"]
+				if encoding != "csv":
+					__ERROR(f"invalid Tile Layer Format: '{encoding}' (only 'CSV' is supported for now) \n")
+				# print("ignored: ", ignored)
+				layer_data.append(data["#text"].replace('\n', '').split(','))
 
-			# print( isinstance(xml["map"]["layer"], list) )
-			if not isinstance(xml["map"]["layer"], list):
-				layers.append(xml["map"]["layer"])
-			else:
-				for l in xml["map"]["layer"]:
-					layers.append(l)
+		# merge down layers
+		for ld in layer_data:
+			for i in range(len(ld)):
+				val = int(ld[i])-1
+				if val >= 0:
+					map_data[i] = val
 
-			# collect tile data from layers
-			layer_data = []
-			for l in layers:
-				ignored = check_ignored_flag(l)
-				if not ignored:
-					data = l["data"]
-					encoding = data["@encoding"]
-					if encoding != "csv":
-						__ERROR(f"invalid Tile Layer Format: '{encoding}' (only 'CSV' is supported for now) \n")
-					# print("ignored: ", ignored)
-					layer_data.append(data["#text"].replace('\n', '').split(','))
+	with open(dest_fname, 'wb') as file:
+		for n in map_data:
+			file.write(n.to_bytes(1, byteorder='big', signed=False))
 
-			# merge down layers
-			for ld in layer_data:
-				for i in range(len(ld)):
-					val = int(ld[i])-1
-					if val >= 0:
-						map_data[i] = val
+	print(f"\n'{dest_fname}' done.")
 
-		with open(dest_file, 'wb') as file:
-			for n in map_data:
-				file.write(n.to_bytes(1, byteorder='big', signed=False))
-	# elif src_ext == ".map"
-		# TODO
 
-	print(f"\n'{dest_file}' done.")
+def map_to_tmx(src_fname, dest_fname):
+	print(f"converting from '{src_fname}' to '{dest_fname}'...")
+	map_data = []
+	with open(src_fname, 'rb') as f:
+		map_str = f.read()
+		map_data = list(map_str)
+
+	for i in range(len(map_data)):
+		map_data[i] = str(map_data[i]+1)
+
+	xml = ""\
+		+'<?xml version="1.0" encoding="UTF-8"?>\n'\
+		+'<map version="1.8" tiledversion="1.8.4" orientation="orthogonal" renderorder="right-down" width="240" height="136" tilewidth="8" tileheight="8" infinite="0" nextlayerid="4" nextobjectid="1">\n'\
+		+' <tileset firstgid="1" source="mini_tiles.tsx"/>\n'\
+		+' <layer id="1" name="Tile Layer 1" width="240" height="136">\n'\
+		+'   <data encoding="csv">\n'\
+		+ ', '.join(map_data) + '\n'\
+		+'   </data>\n'\
+		+' </layer>\n'\
+		+'</map>'
+
+	with open(dest_fname, 'w') as file:
+		file.write(xml)
+
+	print(f"\n'{dest_fname}' done.")
+
+
+def ask_user(msg):
+	while True:
+		inpt = input(msg)
+		if inpt in ['y', 'Y', 'yes', 'yup', 'yoh', 'yeah', 'yea', 'da', 'ya', 'si', 'sim', 'hai', 'yeah yeah yeah', 'sure', 'of course']:
+			return True
+		if inpt in ['n', 'N', 'no', 'nope', 'nah', 'nop', 'newp', 'NO', 'niet', 'nein', 'não', 'nao', 'bo', 'wait no!', 'fu', 'under my dead body!', ]:
+			return False
+
 
 def main(argc, argv):
 	# print("argv[0]: ", argv[0])
 
-	if argc <= 2:
+	if argc == 1:
 		__ERROR("invalid file names\n\n    Usage: tictiled <source_file[.map|.tmx]> <dest_file[.tmx|.map]>\n")
 
 	src_fname = argv[1]
 	dest_fname = argv[2]
 
-	if not (
-	           ( src_fname.endswith(".map") and dest_fname.endswith(".tmx") )\
-	        or ( src_fname.endswith(".tmx") and dest_fname.endswith(".map") )\
-	       ):
+	if not (   (src_fname.endswith(".map") and dest_fname.endswith(".tmx"))\
+	        or (src_fname.endswith(".tmx") and dest_fname.endswith(".map"))  ):
 		__ERROR("invalid file extensions \n")
 
 	if not file_exists(src_fname):
 		__ERROR(f"couldn't load file '{src_fname}'. \n")
 
-	# if file_exists(dest_fname):
+	if file_exists(dest_fname):
 		# TODO: bother asking about overwriting? (add '-y' parameter for skipping it)
+		if not ask_user(f"file {dest_fname} exists: you want to overwrite it? "):
+			return
 
-	convert(src_fname, dest_fname)
+	src_ext  = os.path.splitext(src_fname)[1]
+	dest_ext = os.path.splitext(dest_fname)[1]
+
+	if src_ext == ".tmx":
+		tmx_to_map(src_fname, dest_fname)
+	elif src_ext == ".map":
+		map_to_tmx(src_fname, dest_fname)
 
 
 
